9e1673d7eebdd037bc8befdf12f464275ca66a8c
NitinBakaya <67510136+NitinBakaya@users.noreply.github.com>
2022-10-26 14:53:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.cidr_name
+
+  azs             = var.azs_name
+  private_subnets = var.private_subnets_name
+  public_subnets  = var.public_subnets_name
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
